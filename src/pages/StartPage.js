014dcc3f93375e3f13f8f526c9071d6146d500aa
ELLEN BJORKMAN <ELLEN881008@HOTMAIL.SE>
2021-02-04 09:21:09 +0100

    Adds another section to the startpage. Changes in media queries.

--- src/pages/StartPage.js
+++ src/pages/StartPage.js
@@ -14,20 +14,21 @@ export const StartPage = () => {
             NEW ARRIVALS
           </h1>
           <p>Carefully selected products.</p>
-          <Link to='/products'>
+          <Link to="/products">
             <h2>Shop Products</h2>
           </Link>
         </div>
       </HeroSection>
       <SecondSection>
         <ImageCard text="test3" path="/products" imageUrl="https://res.cloudinary.com/dztqyanvb/image/upload/v1612278091/products/pexels-elika-margaretha-4898516_xuavth.jpg" />
-        <ColorCard text="Sign up today" paragraph="Get 20% off" path="/signup" />
+        <ColorCard text="Sign up today" paragraph="Get 20% off" path="/signup" color="#cdd0cb" />
+        <ColorCard text="The gift guide" paragraph="lorem ipsum" path="/" color="#a59195" />
         <ImageCard text="test" path="/products" imageUrl="https://res.cloudinary.com/dztqyanvb/image/upload/v1611155887/products/pexels-cottonbro-4277097_wig5hy.jpg" />
       </SecondSection>
     </StartPageContainer>
   )
 }
-
+//#91A5A1
 const StartPageContainer = styled.section`
   width: 100%;
 `
@@ -65,12 +66,31 @@ const HeroSection = styled.div`
     font-size: 20px;
   }
 `
+
 const SecondSection = styled.div`
   display: flex;
   flex-direction: column;
   width: 100%;
-  @media (min-width: 1024px){
+  div{
+    :nth-of-type(3){
+        display: none;
+    }
+  }
+  @media (min-width: 700px){
     flex-direction: row;
+    flex-wrap: wrap;
     padding-top: 50px;
+    div{
+      :nth-of-type(3){
+          display: flex;
+      }
+    }
+  }
+  @media (min-width: 1024px){
+    div{
+      :nth-of-type(3){
+          display: none;
+      }
+    }
   }
 `
